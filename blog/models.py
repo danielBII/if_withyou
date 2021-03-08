8c84e2fa5b354452467fa781968f176700f94057
DanielCosta <danielvittor@gmail.com>
2021-03-08 12:04:35 -0300

    commit inicial

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User 
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    summary = RichTextField()
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
