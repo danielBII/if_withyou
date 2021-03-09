97b251a8edf365b92302bfabc0c59133c3f54f0e
DanielCosta <danielvittor@gmail.com>
2021-03-08 23:11:54 -0300

    þ

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,5 @@ class Post(models.Model):
     content = RichTextUploadingField()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     created_at = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.title
--- if_withyou/settings.py
+++ if_withyou/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'if_withyou.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
