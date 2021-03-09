bb30ff1e413e27033e529e3dc4a385a457fec858
DanielCosta <danielvittor@gmail.com>
2021-03-09 13:46:43 -0300

    atualizando

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+
 # Create your views here.
+from blog.models import Post
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {' posts ': posts})
